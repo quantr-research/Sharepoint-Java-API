2fb6d6aeca7bc174de58e90dd9fa71a9be98cae2
Peter <peter@quantr.hk>
2017-10-07 12:26:33 +0800

    dev

--- Sharepoint-Java-API/src/test/java/com/github/mcheung63/test/TestSharePointOnline.java
+++ Sharepoint-Java-API/src/test/java/com/github/mcheung63/test/TestSharePointOnline.java
@@ -1,76 +1,64 @@
-/*
- * Copyright (C) 2017 Peter <peter@quantr.hk>.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
 package com.github.mcheung63.test;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.auth.NTLMSchemeFactory;
-import org.apache.http.impl.client.AbstractHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 public class TestSharePointOnline {
 
 	@Test
 	public void test() throws Exception {
-//		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-//		credsProvider.setCredentials(
-//				new AuthScope("https://quantr.sharepoint.com", 80),
-//				new UsernamePasswordCredentials("peter@quantr.hk", "1qaz2wsx#EDC"));
-//		CloseableHttpClient httpclient = HttpClients.custom()
-//				.setDefaultCredentialsProvider(credsProvider)
-//				.build();
+		CloseableHttpClient httpclient = HttpClients.custom().setRetryHandler(new DefaultHttpRequestRetryHandler(0, false)).build();
 
-		String url = "https://quantr.sharepoint.com";
-		//HttpClient httpclient = HttpClientBuilder.create().build();
-		DefaultHttpClient httpclient = (DefaultHttpClient) new DefaultHttpClient();
+		String user = "guest1@1234.hk";
+		String pwd = "1234";
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(AuthScope.ANY, new NTCredentials(user, pwd, "", ""));
 
-		HttpGet request = new HttpGet(url);
+		HttpHost target = new HttpHost("quantr.sharepoint.com", 443, "https");
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
 
-		((AbstractHttpClient) httpclient).getAuthSchemes().register("ntlm", new NTLMSchemeFactory());
-		CredentialsProvider provider = ((AbstractHttpClient) httpclient).getCredentialsProvider();
-		NTCredentials creds = new NTCredentials("peter@quantr.hk", "1qaz2wsx#EDC", "", "");
+		HttpGet request1 = new HttpGet("/_api/web?$select=Title");
+		CloseableHttpResponse response1 = null;
+		try {
+			response1 = httpclient.execute(target, request1, context);
+			EntityUtils.consume(response1.getEntity());
+			System.out.println("1 : " + response1.getStatusLine().getStatusCode());
+		} finally {
+			if (response1 != null) {
+				response1.close();
+			}
+		}
 
-		provider.setCredentials(AuthScope.ANY, creds);
-		HttpResponse response = httpclient.execute(request);
-
-		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
-
+//		String file = "/Shared%20Documents/1.txt";
+//		HttpGet request2 = new HttpGet("/_api/web/GetFileByServerRelativeUrl('" + file + "')/Etag");
+//		CloseableHttpResponse response2 = null;
 //		try {
-//			HttpGet httpget = new HttpGet("http://quantr.sharepoint.com/_api/web/lists");
-//
-//			System.out.println("Executing request " + httpget.getRequestLine());
-//			CloseableHttpResponse response = httpclient.execute(httpget);
-//			System.out.println("response=" + response);
-//			System.out.println();
-//			try {
-//				System.out.println("----------------------------------------");
-//				System.out.println("statusline=" + response.getStatusLine());
-//				EntityUtils.consume(response.getEntity());
-//			} finally {
-//				response.close();
+//			response2 = httpclient.execute(target, request2, context);
+//			EntityUtils.consume(response2.getEntity());
+//			int rc = response2.getStatusLine().getStatusCode();
+//			String reason = response2.getStatusLine().getReasonPhrase();
+//			if (rc != HttpStatus.SC_OK) {
+//				System.out.println(file + " is missing.  Reason : "
+//						+ reason + "   rc : " + rc + "(500 is the equivalent of NOT FOUND)");
+//			} else {
+//				System.out.println(file + " exists.");
 //			}
 //		} finally {
-//			httpclient.close();
+//			if (response2 != null) {
+//				response2.close();
+//			}
 //		}
 	}
 }
